20a4b200496ca4407d5d15f2a32f244babcb853a
fcdadada <90451274+fcdadada@users.noreply.github.com>
2021-09-17 22:52:57 +0800

    Delete src directory

--- src/main/java/com/fc/cn/tx05/xt04/AirConditioner.java
+++
@@ -1,5 +0,0 @@
-package com.fc.cn.tx05.xt04;
-
-public interface AirConditioner {
-    public void display();
-}
--- src/main/java/com/fc/cn/tx05/xt04/TV.java
+++
@@ -1,7 +0,0 @@
-package com.fc.cn.tx05.xt04;
-
-public interface TV {
-    public void dispaly();
-
-
-}
